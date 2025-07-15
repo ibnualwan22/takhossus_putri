49947e7023c4ef108d0fde80b4b5e95f985e3ca2
ibnualwan22 <allwanecahzobo@gmail.com>
2025-07-16 02:32:45 +0700

    add rute ke login

--- run.py
+++ run.py
@@ -1,17 +1,16 @@
-# run.py
+# run.py (Kode Baru)
 import os
+from flask import redirect, url_for
 from app import create_app, db
-from app.models import Santri, SksMaster, RekapSks # Import semua model
+from app.models import Santri, SksMaster, RekapSks, RekapAbsensi, RekapBukuSadar, User
 
 app = create_app(os.getenv('FLASK_ENV') or 'default')
 
 @app.shell_context_processor
 def make_shell_context():
-    # Memudahkan akses saat menggunakan 'flask shell'
-    return dict(db=db, Santri=Santri, SksMaster=SksMaster, RekapSks=RekapSks)
+    return dict(db=db, Santri=Santri, SksMaster=SksMaster, RekapSks=RekapSks, RekapAbsensi=RekapAbsensi, RekapBukuSadar=RekapBukuSadar, User=User)
 
 @app.route('/')
 def index():
-    return "<h1>Selamat Datang di Proyek Takhossus Putri!</h1><p>Fokus: Menu Rekap</p>"
-
-# Rute untuk admin akan kita buat di file terpisah nanti (app/routes.py)
+    # Langsung alihkan ke dashboard admin
+    return redirect(url_for('admin.admin_dashboard'))
